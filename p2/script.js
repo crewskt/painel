d805713011ffcfd55b96938fd8d68c2a414c76b6
Raul <106830985+crewskt@users.noreply.github.com>
2024-06-25 10:36:23 -0300

    1

--- p2/script.js
+++ p2/script.js
@@ -20,55 +20,10 @@ Vue.filter("toFixed", (num, asset) => {
 });
 
 Vue.filter("toMoney", (num) => {
-  return Number(num)
-    .toFixed(0)
-    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return Number(num).toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 });
 });
 
-// Componente para criar gráfico de linha
-Vue.component("linechart", {
-  props: {
-    width: { type: Number, default: 400, required: true },
-    height: { type: Number, default: 40, required: true },
-    values: { type: Array, default: () => [], required: true },
-  },
-  template: '<canvas :width="width" :height="height"></canvas>',
-  watch: {
-    values: {
-      handler: 'renderChart',
-      deep: true,
-    }
-  },
-  mounted() {
-    this.renderChart();
-  },
-  methods: {
-    renderChart() {
-      const canvas = this.$el;
-      const ctx = canvas.getContext("2d");
-
-      ctx.clearRect(0, 0, this.width, this.height);
-
-      const max = Math.max(...this.values);
-      const min = Math.min(...this.values);
-      const range = max - min || 1;
-      const scaledValues = this.values.map(val => ((val - min) / range) * this.height);
-
-      ctx.beginPath();
-      ctx.moveTo(0, this.height - scaledValues[0]);
-
-      scaledValues.forEach((val, index) => {
-        const x = (index / (scaledValues.length - 1)) * this.width;
-        const y = this.height - val;
-        ctx.lineTo(x, y);
-      });
-
-      ctx.strokeStyle = "#7f8fa4";
-      ctx.stroke();
-    },
-  },
-});
-
+// Adicionando a instância do Vue.js
 new Vue({
   el: "#app",
   data() {
@@ -86,15 +41,15 @@ new Vue({
         order: "asc",
       },
       socket: null,
-      lastCoin: null,
+      lastListedCoin: null, // Adicionando estado para última moeda listada
     };
   },
   created() {
     this.loadCoins();
     this.connectSocket();
     this.loadLSRFromLocalStorage();
-    this.getLastCoinListed();
-    setInterval(this.saveLSRToLocalStorage, 300000);
+    this.loadLongShortRatios(); // Iniciar o carregamento dos ratios long/short
+    setInterval(this.loadLongShortRatios, 300000); // Atualizar a cada 5 minutos
   },
   computed: {
     sortLabel() {
@@ -104,8 +59,10 @@ new Vue({
         assetVolume: "Volume",
         percent: "Percent",
         trades: "Trades",
+        longShortRatio: "Long/Short",
+        volatility: "Volatility",
       };
-      return `Ord: ${keyMap[this.sort.key]}`;
+      return ` ${keyMap[this.sort.key]}`;
     },
     coinsList() {
       let sortedCoins = this.filteredCoins;
@@ -129,7 +86,7 @@ new Vue({
         const response = await fetch("https://fapi.binance.com/fapi/v1/ticker/24hr");
         const data = await response.json();
         this.coins = data.filter(d => d.symbol.endsWith("USDT") && 
-          !["DGBUSDT", "WAVESUSDT", "MDTUSDT","RADUSDT","STRAXUSDT","SLPUSDT","IDEXUSDT","CVXUSDT","SNTUSDT","STPTUSDT","CTKUSDT","GLMRUSDT"].includes(d.symbol))
+          !["DGBUSDT", "WAVESUSDT", "MDTUSDT","RADUSDT","STRAXUSDT","SLPUSDT","IDEXUSDT","CVXUSDT","SNTUSDT","STPTUSDT","CTKUSDT","GLMRUSDT","AGIXUSDT","OCEANUSDT"].includes(d.symbol))
           .map(d => ({
             symbol: d.symbol,
             pair: d.symbol,
@@ -144,13 +101,20 @@ new Vue({
             percent: Number(d.priceChangePercent),
             assetVolume: Number(d.volume),
             trades: Number(d.count),
+            longShortRatio: Number(d.longShortRatio),
+            volatility: Number(d.volatility),
             history: [Number(d.lastPrice) * 0.95, Number(d.lastPrice) * 1.05, Number(d.lastPrice)],
             style: {
               gain: d.priceChange > 0,
               loss: d.priceChange < 0,
             },
             longShortRatio: null,
+            volatility: 0, // Inicialmente 0, você pode calcular isso conforme necessário
           }));
+        
+        // Obter a última moeda listada
+        this.lastListedCoin = this.coins[this.coins.length - 1].symbol;
+
         this.loadLongShortRatios();
         this.status = 1;
       } catch (error) {
@@ -160,24 +124,35 @@ new Vue({
     },
     async loadLongShortRatios() {
       const symbols = this.coins.map(c => c.symbol);
-      const longShortRatios = {};
-
+      
       const fetchLongShortRatio = async (symbol) => {
         try {
           const response = await fetch(`https://fapi.binance.com/futures/data/globalLongShortAccountRatio?symbol=${symbol}&period=5m`);
           if (!response.ok) throw new Error('Failed to fetch');
           const data = await response.json();
-          longShortRatios[symbol] = data.length >= 30 ? Number(data[29].longShortRatio).toFixed(4) : 'N/A';
-          this.saveLSRToLocalStorage();
+          const ratio = data.length >= 30 ? Number(data[29].longShortRatio).toFixed(4) : 'N/A';
+          this.longShortRatios = {
+            ...this.longShortRatios,
+            [symbol]: ratio,
+          };
+          this.updateCoinsWithRatios();
+          this.saveLSRToLocalStorage(); // Salvar no localStorage após a atualização
         } catch (error) {
           console.error(`Failed to load long/short ratio for ${symbol}:`, error);
-          longShortRatios[symbol] = 'N/A';
+          this.longShortRatios = {
+            ...this.longShortRatios,
+            [symbol]: 'N/A',
+          };
         }
       };
 
+      // Initial fetch
       await Promise.all(symbols.map(symbol => fetchLongShortRatio(symbol)));
-      this.longShortRatios = longShortRatios;
-      this.updateCoinsWithRatios();
+
+      // Set interval to update every 5 minutes
+      setInterval(async () => {
+        await Promise.all(symbols.map(symbol => fetchLongShortRatio(symbol)));
+      }, 300000); // 5 minutes in milliseconds
     },
     updateCoinsWithRatios() {
       this.coins = this.coins.map(coin => ({
@@ -188,78 +163,67 @@ new Vue({
     setLimit(limit) {
       this.limit = limit;
     },
-    setAsset(asset) {
+    filterAsset(asset) {
       this.asset = asset;
     },
-    setSort(key) {
-      if (this.sort.key === key) {
-        this.sort.order = this.sort.order === "asc" ? "desc" : "asc";
-      } else {
-        this.sort.key = key;
-        this.sort.order = "asc";
-      }
-    },
-    setSearch(event) {
-      this.search = event.target.value;
-    },
-    async getLastCoinListed() {
-      try {
-        const response = await fetch("https://fapi.binance.com/fapi/v1/exchangeInfo");
-        const data = await response.json();
-        const sortedSymbols = data.symbols.sort((a, b) => new Date(b.onboardDate) - new Date(a.onboardDate));
-        const lastSymbol = sortedSymbols[0];
-        this.lastCoin = {
-          token: lastSymbol.symbol.replace('USDT', ''),
-          icon: `https://betabot.store/icons/${lastSymbol.symbol.replace("USDT", "").toLowerCase()}.png`,
-          onboardDate: new Date(lastSymbol.onboardDate).toLocaleDateString()
-        };
-      } catch (error) {
-        console.error("Failed to fetch last coin listed:", error);
-      }
+    sortBy(key, order) {
+      this.sort.key = key;
+      this.sort.order = order;
     },
     connectSocket() {
-      this.socket = new WebSocket("wss://fstream.binance.com/ws/!ticker@arr");
-      this.socket.onmessage = this.handleSocketMessage;
-      this.socket.onopen = () => console.log("WebSocket connected");
+      const url = "wss://fstream.binance.com/stream";
+      const stream = "!ticker@arr";
+      this.socket = new WebSocket(`${url}?streams=${stream}`);
+
+      this.socket.onopen = () => {
+        this.loaderVisible = false;
+        console.log("WebSocket connection opened.");
+      };
+
+      this.socket.onmessage = (event) => {
+        const data = JSON.parse(event.data).data;
+        this.updateCoinPrices(data);
+      };
+
       this.socket.onclose = () => {
-        console.log("WebSocket disconnected, reconnecting...");
+        console.log("WebSocket connection closed. Reconnecting...");
         setTimeout(this.connectSocket, 1000);
       };
-      this.socket.onerror = (error) => console.error("WebSocket error:", error);
+
+      this.socket.onerror = (error) => {
+        console.error("WebSocket error:", error);
+        this.socket.close();
+      };
     },
-    handleSocketMessage(event) {
-      const data = JSON.parse(event.data);
-      data.forEach(d => {
-        if (d.s.endsWith("USDT")) {
-          const coin = this.coins.find(c => c.symbol === d.s);
-          if (coin) {
-            coin.close = Number(d.c);
-            coin.change = Number(d.p);
-            coin.percent = Number(d.P);
-            coin.assetVolume = Number(d.q);
-            coin.trades = Number(d.n);
-            coin.style.gain = d.p > 0;
-            coin.style.loss = d.p < 0;
-          }
+    updateCoinPrices(data) {
+      this.coins = this.coins.map(coin => {
+        const ticker = data.find(t => t.s === coin.symbol);
+        if (ticker) {
+          const newHistory = coin.history.slice(-29).concat(Number(ticker.c));
+          const volatility = ((ticker.h - ticker.l) / ticker.o) * 100; // Exemplo de cálculo de volatilidade
+          return {
+            ...coin,
+            close: Number(ticker.c),
+            change: Number(ticker.p),
+            percent: Number(ticker.P),
+            assetVolume: Number(ticker.q),
+            trades: Number(ticker.n),
+            history: newHistory,
+            volatility: volatility,
+          };
         }
+        return coin;
       });
     },
     saveLSRToLocalStorage() {
-      const dataToSave = {
-        ratios: this.longShortRatios,
-        timestamp: Date.now(),
-      };
-      localStorage.setItem('longShortRatios', JSON.stringify(dataToSave));
+      const lsrData = JSON.stringify(this.longShortRatios);
+      localStorage.setItem("lsr", lsrData);
     },
     loadLSRFromLocalStorage() {
-      const savedData = localStorage.getItem('longShortRatios');
-      if (savedData) {
-        const parsedData = JSON.parse(savedData);
-        const currentTime = Date.now();
-        if (currentTime - parsedData.timestamp < 3600000) {  // 1 hour
-          this.longShortRatios = parsedData.ratios;
-          this.updateCoinsWithRatios();
-        }
+      const lsrData = localStorage.getItem("lsr");
+      if (lsrData) {
+        this.longShortRatios = JSON.parse(lsrData);
+        this.updateCoinsWithRatios();
       }
     },
   },
